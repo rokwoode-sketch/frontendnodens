6a4586e128e68cb1c97fff65fec874999ce88288
rokwoode-sketch <rokwoode@gmail.com>
2026-06-09 02:16:54 +0100

    update src/pages/HomePage.jsx

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -1,15 +1,7 @@
-import { useLanguage } from '../i18n/LanguageContext';
 import Hero from '../components/Hero';
 import Stats from '../components/Stats';
 import Services from '../components/Services';
-import WhyNodens from '../components/WhyNodens';
-import Treatments from '../components/Treatments';
-import Gallery from '../components/Gallery';
-import Testimonials from '../components/Testimonials';
-import FAQ from '../components/FAQ';
-import Contact from '../components/Contact';
-
-// All schema.org markup
+import HomeExplore from '../components/HomeExplore';
 import SEO from '../components/SEO';
 
 export default function HomePage() {
@@ -19,12 +11,7 @@ export default function HomePage() {
       <Hero />
       <Stats />
       <Services />
-      <WhyNodens />
-      <Treatments />
-      <Gallery />
-      <Testimonials />
-      <FAQ />
-      <Contact />
+      <HomeExplore />
     </>
   );
 }
