6330d9873557cbcb33efa7b83c2798dbb1b53dc0
rokwoode-sketch <rokwoode@gmail.com>
2026-06-09 03:22:25 +0100

    update src/pages/HomePage.jsx

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -1,17 +1,19 @@
-import Hero from '../components/Hero';
-import Stats from '../components/Stats';
-import Services from '../components/Services';
-import HomeExplore from '../components/HomeExplore';
-import SEO from '../components/SEO';
-
-export default function HomePage() {
-  return (
-    <>
-      <SEO />
-      <Hero />
-      <Stats />
-      <Services />
-      <HomeExplore />
-    </>
-  );
-}
+import Hero from '../components/Hero';
+import Stats from '../components/Stats';
+import Services from '../components/Services';
+import Gallery from '../components/Gallery';
+import HomeExplore from '../components/HomeExplore';
+import SEO from '../components/SEO';
+
+export default function HomePage() {
+  return (
+    <>
+      <SEO />
+      <Hero />
+      <Stats />
+      <Services />
+      <Gallery mode="home" />
+      <HomeExplore />
+    </>
+  );
+}
